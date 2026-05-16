4ffb44dc7b14d7fc323ff617931c4eebf42c7438
Igor Glushkov <igr.glushkov@gmail.com>
2026-05-16 14:19:08 +0700

    feat(iotserv_db): delete_device

--- iotserv/src/iotserv_db.erl
+++ iotserv/src/iotserv_db.erl
@@ -1,7 +1,7 @@
 -module(iotserv_db).
 -include("iot_device.hrl").
--export([create_tables/1, close_tables/0, add_device/1, lookup_device_id/1]).
--export([restore_backup/0]).
+-export([create_tables/1, close_tables/0, add_device/1, delete_device/1,
+        lookup_device/1, restore_backup/0]).
 
 create_tables(FileName) ->
     ets:new(iotDeviceRam, [named_table, {keypos, #iot_device.id}]),
@@ -14,6 +14,10 @@ close_tables() ->
 add_device(#iot_device{} = Device) ->
     ets:insert(iotDeviceRam, Device),
     dets:insert(iotDeviceDisk, Device).
+
+delete_device(Id) ->
+    ets:delete(iotDeviceRam, Id),
+    dets:delete(iotDeviceDisk, Id).
 
 lookup_device(Id) ->
     case ets:lookup(iotDeviceRam, Id) of
