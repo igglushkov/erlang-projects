f00e39779cc2834bbd4c4a8644fc4e8e936a6c2f
Igor Glushkov <igr.glushkov@gmail.com>
2026-05-16 13:55:01 +0700

    feat(iotserv_db): restore_backup

--- iotserv/src/iotserv_db.erl
+++ iotserv/src/iotserv_db.erl
@@ -1,6 +1,7 @@
 -module(iotserv_db).
 -include("iot_device.hrl").
 -export([create_tables/1, close_tables/0, add_device/1, lookup_device_id/1]).
+-export([restore_backup/0]).
 
 create_tables(FileName) ->
     ets:new(iotDeviceRam, [named_table, {keypos, #iot_device.id}]),
@@ -19,3 +20,10 @@ lookup_device_id(DeviceId) ->
         [Device] -> {ok, Device};
         []       -> {error, instance}
     end.
+
+restore_backup() ->
+    Insert = fun(#iot_device{} = Device) ->
+        ets:insert(iotDeviceRam, Device),
+        continue
+        end,
+    dets:traverse(iotDeviceDisk, Insert).
