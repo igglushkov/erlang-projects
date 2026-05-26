be73366359bd47b4cf702e9f3650e4b0418c7f79
Igor Glushkov <igr.glushkov@gmail.com>
2026-05-26 18:04:58 +0700

    feat: basic logic

--- code_lock/src/code_lock.erl
+++ code_lock/src/code_lock.erl
@@ -4,6 +4,7 @@
 
 -export([start_link/1]).
 -export([button/1]).
+-export([open/3, locked/3]).
 -export([init/1, callback_mode/0, terminate/3]).
 
 start_link(Code) ->
@@ -22,6 +23,34 @@ callback_mode() ->
 terminate(_Reason, State, _Data) ->
     State =/= locked andalso do_lock(),
     ok.
+
+locked(enter, _OldState, _Data) ->
+    do_lock(),
+    keep_state_and_data;
+
+locked(state_timeout, _OldState, _Data) ->
+    do_lock(),
+    keep_state_and_data;
+
+locked(cast, {button, Button}, 
+    #{code := Code, buttons := Buttons} = Data) ->
+    NewButtons = Buttons ++ [Button],
+    if NewButtons == Code ->
+        {next_state, open, Data#{buttons => []},
+        [{state_timeout, 10_000, lock}]};
+    true ->
+        {keep_state, Data#{buttons => NewButtons}}
+    end.
+
+open(enter, locked, _Data) ->
+    do_unlock(),
+    keep_state_and_data;
+
+open(cast, {button, _}, _Data) ->
+    keep_state_and_data;
+
+open(state_timeout, lock, Data) ->
+    {next_state, locked, Data}.
 
 do_unlock() ->
     io:format("Unlocked~n").
