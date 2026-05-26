da613bb5130b7d86fc4d8e5892e84f82394073dd
Igor Glushkov <igr.glushkov@gmail.com>
2026-05-26 19:25:48 +0700

    feat: add state "suspended"

--- code_lock/src/code_lock.erl
+++ code_lock/src/code_lock.erl
@@ -3,9 +3,11 @@
 -define(NAME, code_lock).
 
 -export([start_link/1]).
--export([button/1]).
--export([open/3, locked/3]).
+-export([button/1, verify/0]).
+-export([open/3, locked/3, suspended/3]).
 -export([init/1, callback_mode/0, terminate/3]).
+
+-define(MAX_ATTEMPTS, 3).
 
 start_link(Code) ->
     gen_statem:start_link({local, ?NAME}, ?MODULE, Code, []).
@@ -13,8 +15,11 @@ start_link(Code) ->
 button(Button) ->
     gen_statem:cast(?NAME, {button, Button}).
 
+verify() ->
+    gen_statem:cast(?NAME, verify).
+
 init(Code) ->
-    Data = #{code => Code, length => length(Code), buttons => []},
+    Data = #{code => Code, length => length(Code), attempts => 0, buttons => []},
     {ok, locked, Data}.
 
 callback_mode() ->
@@ -32,14 +37,20 @@ locked(state_timeout, _OldState, _Data) ->
     do_lock(),
     keep_state_and_data;
 
-locked(cast, {button, Button}, 
-    #{code := Code, buttons := Buttons} = Data) ->
-    NewButtons = Buttons ++ [Button],
-    if NewButtons == Code ->
-        {next_state, open, Data#{buttons => []},
+locked(cast, {button, Button}, #{buttons := Buttons} = Data) ->
+    {keep_state, Data#{buttons => Buttons ++ [Button]}};
+
+locked(cast, verify, 
+    #{code := Code, attempts := Attempts, buttons := Buttons} = Data) ->
+    if Buttons == Code ->
+        {next_state, open, Data#{buttons => [], attempts => 0},
         [{state_timeout, 10_000, lock}]};
     true ->
-        {keep_state, Data#{buttons => NewButtons}}
+        if Attempts + 1 == ?MAX_ATTEMPTS ->
+            {next_state, suspended, Data, [{state_timeout, 10_000, lock}]};
+        true ->
+            {keep_state, Data#{attempts => Attempts + 1}}
+        end
     end.
 
 open(enter, locked, _Data) ->
@@ -51,6 +62,20 @@ open(cast, {button, _}, _Data) ->
 
 open(state_timeout, lock, Data) ->
     {next_state, locked, Data}.
+
+suspended(enter, locked, _Data) ->
+    do_suspend(),
+    keep_state_and_data;
+
+suspended(cast, _EventData, _Data) ->
+    io:format("In suspeneded mode~n"),
+    keep_state_and_data;
+
+suspended(state_timeout, lock, Data) ->
+    {next_state, locked, Data#{buttons => [], attempts => 0}}.
+
+do_suspend() ->
+    io:format("Suspended~n").
 
 do_unlock() ->
     io:format("Unlocked~n").
