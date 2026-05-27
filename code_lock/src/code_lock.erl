50d01ac2a76726482dc9b3e19f60f6d44d5bf837
Igor Glushkov <igr.glushkov@gmail.com>
2026-05-27 16:54:54 +0700

    feat: add state() function

--- code_lock/src/code_lock.erl
+++ code_lock/src/code_lock.erl
@@ -3,7 +3,7 @@
 -define(NAME, code_lock).
 
 -export([start_link/1]).
--export([button/1, verify/0, clear/0, change/2]).
+-export([button/1, verify/0, clear/0, change/2, state/0]).
 -export([show/0]).
 -export([open/3, locked/3, suspended/3]).
 -export([init/1, callback_mode/0, terminate/3]).
@@ -35,6 +35,9 @@ clear() ->
 show() ->
     gen_statem:call(?NAME, show).
 
+state() ->
+    gen_statem:call(?NAME, state).
+
 init(Code) ->
     Data = #{code => Code, length => length(Code), attempts => 0, buttons => []},
     {ok, locked, Data}.
@@ -45,6 +48,9 @@ callback_mode() ->
 terminate(_Reason, State, _Data) ->
     State =/= locked andalso do_lock(),
     ok.
+
+%%
+%% locked state
 
 locked(enter, _OldState, _Data) ->
     do_lock(),
@@ -60,12 +66,6 @@ locked(cast, {button, Button}, #{buttons := Buttons} = Data) ->
 locked(cast, clear, Data) ->
     {keep_state, Data#{buttons => []}};
 
-locked({call, From}, show, #{buttons := Buttons}) ->
-    {keep_state_and_data, [{reply, From, Buttons}]};
-
-locked({call, From}, {change, _, _}, _Data) ->
-    {keep_state_and_data, [{reply, From, operation_is_forbidden}]};
-
 locked(cast, verify, 
     #{code := Code, buttons := Buttons} = Data) when Buttons =:= Code ->
     {next_state, open, Data#{buttons => [], attempts => 0}, [{state_timeout, 10_000, lock}]};
@@ -74,11 +74,26 @@ locked(cast, verify, #{attempts := Attempts} = Data) when Attempts + 1 == ?MAX_A
         {next_state, suspended, Data, [{state_timeout, 10_000, lock}]};
 
 locked(cast, verify, #{attempts := Attempts} = Data) ->
-    {keep_state, Data#{attempts => Attempts + 1}}.
+    {keep_state, Data#{attempts => Attempts + 1}};
+
+locked({call, From}, show, #{buttons := Buttons}) ->
+    {keep_state_and_data, [{reply, From, Buttons}]};
+
+locked({call, From}, {change, _, _}, _Data) ->
+    {keep_state_and_data, [{reply, From, operation_is_forbidden}]};
+
+locked({call, From}, state, _Data) ->
+    {keep_state_and_data, [{reply, From, locked}]}.
+
+%%
+%% open state
 
 open(enter, locked, _Data) ->
     do_unlock(),
     keep_state_and_data;
+
+open(state_timeout, lock, Data) ->
+    {next_state, locked, Data};
 
 open(cast, {button, _}, _Data) ->
     keep_state_and_data;
@@ -90,12 +105,18 @@ open({call, From}, {change, OldCode, NewCode}, #{code := Code} = Data) when OldC
 open({call, From}, {change, _OldCode, _NewCode}, _Data) ->
     {keep_state_and_data, [{reply, From, incorrect_old_code}]};
 
-open(state_timeout, lock, Data) ->
-    {next_state, locked, Data}.
+open({call, From}, state, _Data) ->
+    {keep_state_and_data, [{reply, From, open}]}.
+
+%%
+%% suspended state
 
 suspended(enter, locked, _Data) ->
     do_suspend(),
     keep_state_and_data;
+
+suspended(state_timeout, lock, Data) ->
+    {next_state, locked, Data#{buttons => [], attempts => 0}};
 
 suspended(cast, _EventData, _Data) ->
     io:format("In suspeneded mode~n"),
@@ -104,8 +125,8 @@ suspended(cast, _EventData, _Data) ->
 suspended({call, From}, {change, _, _}, _Data) ->
     {keep_state_and_data, [{reply, From, operation_is_forbidden}]};
 
-suspended(state_timeout, lock, Data) ->
-    {next_state, locked, Data#{buttons => [], attempts => 0}}.
+suspended({call, From}, state, _Data) ->
+    {keep_state_and_data, [{reply, From, suspended}]}.
 
 do_suspend() ->
     io:format("Suspended~n").
