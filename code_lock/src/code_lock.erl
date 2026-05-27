1a611f70da4bc103cd03eeea91eab3173d346429
Igor Glushkov <igr.glushkov@gmail.com>
2026-05-27 12:37:49 +0700

    feat: add change() function

--- code_lock/src/code_lock.erl
+++ code_lock/src/code_lock.erl
@@ -3,18 +3,28 @@
 -define(NAME, code_lock).
 
 -export([start_link/1]).
--export([button/1, verify/0, clear/0]).
+-export([button/1, verify/0, clear/0, change/2]).
 -export([show/0]).
 -export([open/3, locked/3, suspended/3]).
 -export([init/1, callback_mode/0, terminate/3]).
 
 -define(MAX_ATTEMPTS, 3).
+-define(IS_VALID_CODE(Code), is_list(Code), length(Code) > 0).
 
-start_link(Code) ->
-    gen_statem:start_link({local, ?NAME}, ?MODULE, Code, []).
+start_link(Code) when ?IS_VALID_CODE(Code) ->
+    gen_statem:start_link({local, ?NAME}, ?MODULE, Code, []);
+
+start_link(_Code) ->
+    io:format("Incorrect code format or length~n").
 
 button(Button) ->
     gen_statem:cast(?NAME, {button, Button}).
+
+change(OldCode, NewCode) when ?IS_VALID_CODE(OldCode), ?IS_VALID_CODE(NewCode) ->
+    gen_statem:call(?NAME, {change, OldCode, NewCode});
+
+change(_OldCode, _NewCode) ->
+    io:format("Invalid code format or length~n").
 
 verify() ->
     gen_statem:cast(?NAME, verify).
@@ -53,6 +63,9 @@ locked(cast, clear, Data) ->
 locked({call, From}, show, #{buttons := Buttons}) ->
     {keep_state_and_data, [{reply, From, Buttons}]};
 
+locked({call, From}, {change, _, _}, _Data) ->
+    {keep_state_and_data, [{reply, From, operation_is_forbidden}]};
+
 locked(cast, verify, 
     #{code := Code, attempts := Attempts, buttons := Buttons} = Data) ->
     if Buttons == Code ->
@@ -73,6 +86,13 @@ open(enter, locked, _Data) ->
 open(cast, {button, _}, _Data) ->
     keep_state_and_data;
 
+open({call, From}, {change, OldCode, NewCode}, #{code := Code} = Data) ->
+    if OldCode =:= Code ->
+        {keep_state, Data#{code => NewCode, length => length(NewCode)}, [{reply, From, code_changed}]};
+    true ->
+        {keep_state, Data, [{reply, From, incorrect_old_code}]}
+    end;
+
 open(state_timeout, lock, Data) ->
     {next_state, locked, Data}.
 
@@ -83,6 +103,9 @@ suspended(enter, locked, _Data) ->
 suspended(cast, _EventData, _Data) ->
     io:format("In suspeneded mode~n"),
     keep_state_and_data;
+
+suspended({call, From}, {change, _, _}, _Data) ->
+    {keep_state_and_data, [{reply, From, operation_is_forbidden}]};
 
 suspended(state_timeout, lock, Data) ->
     {next_state, locked, Data#{buttons => [], attempts => 0}}.
