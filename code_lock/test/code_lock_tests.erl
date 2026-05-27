57f4b8c30d872674eeb85d5cc374f1eeca987cf9
Igor Glushkov <igr.glushkov@gmail.com>
2026-05-27 19:59:20 +0700

    feat: add basic tests

---
+++ code_lock/test/code_lock_tests.erl
@@ -0,0 +1,71 @@
+-module(code_lock_tests).
+-include_lib("eunit/include/eunit.hrl").
+
+open_on_correct_code_test() ->
+    code_lock:start_link([1]),
+    code_lock:button(1),
+    code_lock:verify(),
+    
+    ?assertEqual(open, code_lock:state()),
+    
+    code_lock:stop().
+
+stay_locked_on_incorrect_code_test() ->
+    code_lock:start_link([1]),
+    code_lock:button(2),
+    code_lock:verify(),
+
+    ?assertEqual(locked, code_lock:state()),
+    
+    code_lock:stop().
+
+
+suspended_after_three_incorrect_attempts_test() ->
+    code_lock:start_link([1]),
+    code_lock:button(2),
+
+    code_lock:verify(),
+    code_lock:verify(),
+    code_lock:verify(),
+
+    ?assertEqual(suspended, code_lock:state()),
+    
+    code_lock:stop().
+
+
+can_change_code_in_open_state_if_old_code_is_correct_test() ->
+    code_lock:start_link([1]),
+    code_lock:button(1),
+
+    code_lock:verify(),
+
+    ?assertEqual(open, code_lock:state()),
+    ?assertMatch({ok, _}, code_lock:change([1], [2])),
+    ?assertMatch({error, _Reason}, code_lock:change([3], [2])),
+    
+    code_lock:stop().
+
+cannot_change_code_in_locked_state_test() ->
+    code_lock:start_link([1]),
+    code_lock:button(2),
+
+    code_lock:verify(),
+
+    ?assertEqual(locked, code_lock:state()),
+    ?assertMatch({error, _Reason}, code_lock:change([1], [2])),
+    
+    code_lock:stop().
+
+
+cannot_change_code_in_suspended_state_test() ->
+    code_lock:start_link([1]),
+    code_lock:button(2),
+
+    code_lock:verify(),
+    code_lock:verify(),
+    code_lock:verify(),
+
+    ?assertEqual(suspended, code_lock:state()),
+    ?assertMatch({error, _Reason}, code_lock:change([1], [2])),
+    
+    code_lock:stop().
