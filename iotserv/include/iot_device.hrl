c58c6ec4daac642c90a4b55c1d1bf1e9ea0e39f9
Igor Glushkov <igr.glushkov@gmail.com>
2026-05-31 18:56:04 +0700

    feat(iot_device): define type for each property

--- iotserv/include/iot_device.hrl
+++ iotserv/include/iot_device.hrl
@@ -1,4 +1,21 @@
 -export_macros([is_device_property]).
+-export_type([id/0, name/0, address/0, tempature/0, indicators/0, dev_property/0]).
+
+-type id() :: pos_integer().
+-type name() :: term().
+-type address() :: term().
+-type tempature() :: number().
+-type indicators() :: [{term(), term()}].
+
+-type dev_property() :: name | address | tempature | indicators.
+
 -define(is_device_property(P), 
             P == name; P == address; P == tempature; P == indicators).
 
+%% @doc iot_device definition
+
+-record(iot_device, {id :: id(), 
+                    name :: name(),
+                    address :: address(),
+                    tempature :: tempature(),
+                    indicators :: indicators()}).
