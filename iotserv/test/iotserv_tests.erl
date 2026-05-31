eefbd52b29044b0826386a4b7a114a5d6a9cd6b6
Igor Glushkov <igr.glushkov@gmail.com>
2026-05-31 16:44:34 +0700

    feat(tests) : change_device

--- iotserv/test/iotserv_tests.erl
+++ iotserv/test/iotserv_tests.erl
@@ -6,11 +6,30 @@ init_device(Id, Name, Address, Tempature, Indicators) ->
     #iot_device{id = Id, name = Name, address = Address,
                     tempature = Tempature, indicators = Indicators}.
 
-add_device_test() ->
+add_lookup_delete_device_test() ->
     iotserv:start_link(),
-    iotserv:add(1, test_name, home, 28, [{water_consumption, 30}]),
+    Id = 1,
+    iotserv:add(Id, test_name, home, 28, [{water_consumption, 30}]),
     Expected = init_device(1, test_name , home, 28, [{water_consumption, 30}]),
 
     ?assertMatch({ok, Expected}, iotserv:lookup(1)),
 
+    iotserv:delete(Id),
+
+    ?assertMatch({error, instance}, iotserv:lookup(Id)).
+
+change_device_test() ->
+    Id = 1,
+    iotserv:add(Id, test_name, home, 28, [{water_consumption, 30}]),
+
+    Expected = init_device(Id, test_name , home, 28, [{water_consumption, 30}]),
+    ?assertMatch({ok, Expected}, iotserv:lookup(Id)),
+
+    NewIndicators = [{water_consumption, 45}],
+    iotserv:change(Id, indicators, NewIndicators),
+    NewExpected = Expected#iot_device{indicators =  NewIndicators},
+    
+    ?assertMatch({ok, NewExpected}, iotserv:lookup(Id)),
+
+    iotserv:delete(Id),
     iotserv:stop().
